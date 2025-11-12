c0023f2555384e304b5860e9f2ccf9740e08a619
TwelveStudent47 <laczkokevin60@gmail.com>
2025-11-12 12:31:56 +0100

    Finished project

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from netmiko import ConnectHandler
+import datetime as time
 
 def router_start(ip):
     router = {
@@ -15,4 +16,43 @@ def router_start(ip):
     return router_connection
 
 def router_end(router_connection):
-    router_connection.end()
+    router_connection.disconnect()
+
+def save_default_config(ip):
+    router_connection = router_start(ip)
+    default_config = router_connection.send_command("show startup-config")
+
+    with open("default_config.txt", "w") as file:
+        file.writelines(default_config)
+
+    router_end(router_connection)
+
+    print("Sikeresen lementetted az alapértelmezett konfigurációt")
+
+def save_current_config(ip):
+    now = time.datetime.now()
+
+    formatted_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+    config_name = f"config_{formatted_time}.txt"
+    router_connection = router_start(ip)
+    current_config = router_connection.send_command("show running-config")
+
+    with open(config_name, "w") as file:
+        file.writelines(current_config)
+
+    router_end(router_connection)
+
+    print(f"Sikeresen lementetted a jelenleg futó configot ebbe a fájlba: {config_name}")
+
+def run_custom_code(ip, list_of_commands):
+    router_connection = router_start(ip)
+
+    router_connection.send_config_set(list_of_commands)
+
+    config = router_connection.send_command("show running-config")
+
+    print(config)
+
+    router_end(router_connection)
+
+    print("Sikeresen lefuttattad a kódot.")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import netmiko
+from functions import save_default_config, save_current_config, run_custom_code
+
 def main():
+    user_ip = input("Adj meg a routered IP címét: ")
     print("Melyik feladatot szeretnéd végrehajtani?:")
     print("1. Alap konfiguráció letöltése\n"
-          + "2. Aktuális konfiguráció mentése\n",
+          + "2. Aktuális konfiguráció mentése\n"
           + "3. Egyedi konfig betöltése az eszközön\n"
           + "4. Egyedi kód futtatása\n"
           + "5. DHCP konfig beállítása net nélkül\n"
@@ -10,17 +14,41 @@ def main():
     user_choice = user_choice[0]
     match user_choice:
         case "1":
-            print("Alap konfig elmentve...")
+            save_default_config(user_ip)
         case "2":
-            print("Aktuális konfig elmentve...")
+            save_current_config(user_ip)
         case "3":
-            print("Egyedi konfig beállítva...")
+            user_config = input("Írd be a futtatni kívánt config nevét: ")
+            with open(user_config, "r") as file:
+                commands = file.readlines()
+            run_custom_code(user_ip, commands)
         case "4":
-            print("Egyedi kód lefuttatva...")
+            commands = [
+                "int s0/1/0",
+                "ip add 10.0.0.1 255.255.255.0",
+                "no sh"
+            ]
+            run_custom_code(user_ip, commands)
         case "5":
-            print("DHCP konfig beállítva net nélkül...")
+            print("Melyiket szeretnéd betölteni?:")
+            print("1. DHCP net nélkül, NAT-ból"
+                  + "2. DHCP alapkonfig")
+            user_second_choice = input("Feladat száma: ")
+            match user_second_choice:
+                case "1":
+                    with open("dhcp_from_nat_without_internet", "r") as file:
+                        commands = file.readlines()
+                    run_custom_code(user_ip, commands)
+                case "2":
+                    with open("dhcp_without_internet.txt", "r") as file:
+                        commands = file.readlines()
+                    run_custom_code(user_ip, commands)
+                case _:
+                    print("Nem megfelelő számot adtál meg.")
         case "6":
-            print("DHCP konfig beállítva nettel....")
+            with open("dhcp_with_internet.txt", "r") as file:
+                commands = file.readlines()
+            run_custom_code(user_ip, commands)
         case _:
             print("Nem megfelelő számot adtál meg.")
 
